60b00ec84738a56baf6e7342e5fdda079a180d8e
Vipin Suthar <vipin29suthar@gmail.com>
2023-01-06 23:08:00 +0530

    config

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 module.exports = {
     PORT: process.env.PORT || 8000,
-    MONGO_CONNECTION_URL: process.env.MONGO_CONNECTION_URL || "mongodb://localhost:27017",
     DEST: process.env.DEST || "public/upload/",
     UPLOAD_FILES_LIST: process.env.UPLOAD_FILES_LIST || "list.txt"
 }
